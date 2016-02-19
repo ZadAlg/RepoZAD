28e312d601c974801429b916d4882989f82d8a89
ZadAlg <zalg@i.ua>
2016-02-19 18:17:22 +0200

    печать

--- src/date160219/test.java
+++ src/date160219/test.java
@@ -4,4 +4,8 @@ package date160219;
  * Created by user on 19.02.2016.
  */
 public class test {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println();
+    }
 }
